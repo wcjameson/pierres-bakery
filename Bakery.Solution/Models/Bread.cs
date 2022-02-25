352cee926a6b15b371d63b9fec492b2ee64ecc33
wcjameson <williamjameson90@gmail.com>
2022-02-25 13:03:39 -0800

    add code to CalculatePastryPrice passing string to int conversion test

--- Bakery.Solution/Models/Bread.cs
+++ Bakery.Solution/Models/Bread.cs
@@ -4,6 +4,44 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public string BreadAmount { get; set; }
+    public int Price { get; set; }
+
+    public Bread(string breadAmount)
+    {
+      BreadAmount = breadAmount;
+      Price = CalculateBreadPrice(breadAmount);
+    }
+
+    private int CalculateBreadPrice(string breadAmount)
+    {
+      int numberOfLoafs = int.Parse(breadAmount);
+      // return numberOfLoafs;
+      if (numberOfLoafs == 1)
+      {
+        return 5;
+      }
+      else if (numberOfLoafs == 2)
+      {
+        return 10;
+      }
+      else if (numberOfLoafs % 3 == 0)
+      {
+        int discountedLoafs = numberOfLoafs / 3; 
+        return (numberOfLoafs - discountedLoafs) * 5;
+      }
+      else if (numberOfLoafs % 3 == 1)
+      {
+        return (numberOfLoafs * 5) - 10;
+      }
+      else if (numberOfLoafs % 3 == 2)
+      {
+        return (numberOfLoafs * 5) - 5;
+      }
+      else
+      {
+        return numberOfLoafs;
+      }
+    }
   }
 }
--- Bakery.Solution/Models/Pastry.cs
+++ Bakery.Solution/Models/Pastry.cs
@@ -4,6 +4,19 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-    
+    public string PastryAmount { get; set; }
+    public int Price { get; set; }
+
+    public Pastry(string pastryAmount)
+    {
+      PastryAmount = pastryAmount;
+      Price = CalculatePastryPrice(PastryAmount);
+    }
+
+    private int CalculatePastryPrice(string pastryAmount)
+    {
+      int numberOfPastries = int.Parse(pastryAmount);
+      return numberOfPastries;
+    }
   }
 }
