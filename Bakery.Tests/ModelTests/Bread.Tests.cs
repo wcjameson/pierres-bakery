17d0f93f1b3c50df2e25acdd16f1bbdafdd0b470
wcjameson <williamjameson90@gmail.com>
2022-02-25 11:53:51 -0800

    add test coverage for logic branch of CalculateBreadPrice

--- Bakery.Tests/ModelTests/Bread.Tests.cs
+++ Bakery.Tests/ModelTests/Bread.Tests.cs
@@ -17,13 +17,19 @@ namespace Bakery.Tests
     {
       
       Bread newBread = new Bread("3");
-      Assert.AreEqual(3, newBread.Price);
+      Assert.AreEqual(10, newBread.Price);
     }
     [TestMethod]
-    public void CalculateBreadPrice_TallyBreadPrice_BreadPrice()
+    public void CalculateBreadPrice_TallyBreadPriceIfRemainder1_BreadPrice()
     {
-      Bread newLoaf = new Bread("6");
-      Assert.AreEqual(20, newLoaf.Price);
+      Bread newLoaf = new Bread("16");
+      Assert.AreEqual(70, newLoaf.Price);
+    }
+    [TestMethod]
+    public void CalculateBreadPrice_TallyBreadPriceIfRemainder2_BreadPrice()
+    {
+      Bread anotherLoaf = new Bread("17");
+      Assert.AreEqual(80, anotherLoaf.Price);
     }
   }
 }
